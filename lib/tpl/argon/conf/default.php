5944bbf58e60babf9301bb5dc1d02d74709af7dd
master <info@jurjen.dev>
2021-10-18 20:51:37 +0000

    Wiki backup 2021-10-18-1634590297

---
+++ lib/tpl/argon/conf/default.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * default configuration settings
+ *
+ */
+
+$conf['discussionPage']   = 'discussion:@ID@';
+$conf['userPage']         = 'user:@USER@:';
+$conf['hideTools']        = 0;
+$conf['movePageTools']    = 0; 
+$conf['showIcon']         = 0;
---
+++ lib/tpl/argon/conf/metadata.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * configuration metadata
+ *
+ */
+
+$meta['discussionPage']   = array('string');
+$meta['userPage']         = array('string');
+$meta['hideTools']        = array('onoff');
+$meta['movePageTools']    = array('onoff'); 
+$meta['showIcon']         = array('onoff');
